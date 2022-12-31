f3abeefbadba65702b393535ea9d8c63d65ae466
groovy2040 <groovyq1986@gmail.com>
2023-01-01 03:11:44 +1100

    commit

--- screens/Login.js
+++ screens/Login.js
@@ -1,70 +1,77 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { Text, Button, View, TextInput } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Formik } from 'formik';
-
-import { 
-    InnerContainer,
-    StyledContainer,
-    PageLogo,
-    PageTitle,
-    Subtitle,
-    StyledFormArea,
-    TextBoxStyle
-} from '../components/styles';
-
-/*
-    This function is the home page for the app.
-    Create Account - Navigates to the account creation page.
-    Login - If the login details are correct, it will log into the app and navigate to the modules page.
-    Submit Invite ID - After submitting a valid invite ID, the user is navigated to the start of the report.
-    PageLogo - Change the source for your specified image.
-*/
-function Login({ navigation }) {
-    return (
-        <StyledContainer>
-            <StatusBar style="dark" />
-            <InnerContainer>
-                <PageLogo resizeMode="cover" source={require('./../assets/favicon.png')} />
-                <PageTitle>Good Day App</PageTitle>
-                <Text>{'\n'}</Text>
-                <Button title="Create Account" onPress={() => navigation.navigate('Create an Account')}/>
-                <Text>{'\n'}</Text>
-                <Button title="Login" onPress={() => navigation.navigate('Reports')}/>
-                <Text>{'\n'}</Text>
-                <TextInput
-                    style={TextBoxStyle.input}
-                    numeric
-                    placeholder="Enter your Invite ID here"
-                    keyboardType="numeric"
-                    maxLength={6} />
-                <Text>{'\n'}</Text>
-                <Button title="Submit Invite ID" onPress={() => navigation.navigate('Form Start')}/>
-                <Formik
-                    initialValues={{email: '', password: ''}}
-                    onSubmit={(values) => {
-                        console.log(values);
-                    }}
-                />
-            </InnerContainer>
-        </StyledContainer>
-        
-    
-    );
-}
-
-const Stack = createNativeStackNavigator();
-
-function App() {
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+export default function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Email."
+          placeholderTextColor="#003f5c"
+          onChangeText={(email) => setEmail(email)}
+        /> 
+      </View> 
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password."
+          placeholderTextColor="#003f5c"
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+        /> 
+      </View> 
+      <TouchableOpacity>
+        <Text style={styles.forgot_button}>Forgot Password?</Text> 
+      </TouchableOpacity> 
+      <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.loginText}>LOGIN</Text> 
+      </TouchableOpacity> 
+    </View> 
   );
 }
-
-export default Login;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputView: {
+    backgroundColor: "#FFC0CB",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+    alignItems: "center",
+  },
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+  },
+  forgot_button: {
+    height: 30,
+    marginBottom: 30,
+  },
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#FF1493",
+  },
+});
