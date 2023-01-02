6ba54e8b48ab057ac9544562974041c304e83ae6
groovy2040 <groovyq1986@gmail.com>
2023-01-02 17:09:03 +1100

    commit

--- components/styles.js
+++ components/styles.js
@@ -98,3 +98,39 @@ export const CheckBox = StyleSheet.create({
     margin: 8,
   },
 });
+
+export const designs = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputView: {
+    backgroundColor: "#FFC0CB",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+    alignItems: "center",
+  },
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+  },
+  forgot_button: {
+    height: 30,
+    marginBottom: 30,
+  },
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#FF1493",
+  },
+});
